137099333e96b7f90d0338b73387c754a3d663e3
KillianBelet <89791697+KillianBelet@users.noreply.github.com>
2025-04-18 16:30:16 +0200

    ajout des dernieres pages

--- skeleton/src/Controller/HomeController.php
+++ skeleton/src/Controller/HomeController.php
@@ -37,6 +37,18 @@ final class HomeController extends AbstractController
         ]);
     }
 
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
+    }
+
+    #[Route('/mon-compte', name: 'app_account')]
+    public function account(): Response
+    {
+        return $this->render('home/account.html.twig');
+    }
+
     #[Route('/about', name: 'app_about')]
     public function about(): Response
     {
